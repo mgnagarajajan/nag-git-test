7d8d0e274d285d4e9c4f0ce56ed5e8f2b4931d2f
nag <nagarajan.hg@gmail.com>
2023-07-20 23:25:02 -0500

    initial commit

---
+++ src/main/java/com/git/naggittest/NagGitTestApplication.java
@@ -0,0 +1,16 @@
+package com.git.naggittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagGitTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(NagGitTestApplication.class, args);
+
+		System.out.println("Test");
+	}
+
+}
---
+++ src/test/java/com/git/naggittest/NagGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.naggittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagGitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
